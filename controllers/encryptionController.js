c72888721cf00a2a188961f77026287b78a34245
Julian <julian94ellis@gmail.com>
2023-11-28 20:15:08 -0500

    Decrytpion concat iv and ciphertext. Responses in JSON

--- controllers/encryptionController.js
+++ controllers/encryptionController.js
@@ -3,10 +3,16 @@ const AppEncryptor = require('../libs/encryptor');
 exports.encrypt = async (req, res) => {
     let appEncryptor = new AppEncryptor();
     appEncryptor.encrypt(req.params.input);
-    res.status(200).json(appEncryptor.encryptionResult);
+    res.status(200).send(appEncryptor.encryptionResultConcat);
 }
-
 exports.decrypt = async (req, res) => {
     let appEncryptor = new AppEncryptor;
-    res.status(200).send(appEncryptor.decrypt(req.params.input));
+    try {
+        res.status(200).send(appEncryptor.decrypt(req.params.input));
+    } catch (error) {
+        res.status(404).json({
+            error: "Bad Request",
+            message: error.message
+        });
+    }
 }
--- libs/encryptor.js
+++ libs/encryptor.js
@@ -1,8 +1,8 @@
 const crypto = require('crypto');
 class AppEncryptor {
-    secretKey = "57bKEtwMFu/xa6P5QZFlns6ZQV2s1HVXJZWRxCAoIvpMo7cVToh07/zNklJLwB/E";
     encryptionResult;
-
+    encryptionResultConcat;
+    secretKey = "57bKEtwMFu/xa6P5QZFlns6ZQV2s1HVXJZWRxCAoIvpMo7cVToh07/zNklJLwB/E";
     algorithm = 'aes-256-cbc'
     iv = crypto.randomBytes(16);
     key = crypto.createHash('sha512').update(this.secretKey, 'utf-8').digest('hex').substr(0, 32);
@@ -12,14 +12,13 @@ class AppEncryptor {
         let encrypted = cipher.update(plainText);
         encrypted = Buffer.concat([encrypted, cipher.final()]);
         this.encryptionResult = { iv: this.iv.toString('hex'), encryptedData: encrypted.toString('hex') };
-             
+        this.encryptionResultConcat = ''.concat(this.encryptionResult.iv, this.encryptionResult.encryptedData);
     }
 
     decrypt(cipherText) {
-        cipherText = JSON.parse(cipherText);
-        let iv = Buffer.from(cipherText.iv, 'hex');
+        let iv = Buffer.from(cipherText.slice(0,32), 'hex');
         let decipher = crypto.createDecipheriv(this.algorithm, Buffer.from(this.key), iv);
-        let decrypted = decipher.update(Buffer.from(cipherText.encryptedData, 'hex'));
+        let decrypted = decipher.update(Buffer.from(cipherText.slice(32), 'hex'));
         decrypted = Buffer.concat([decrypted, decipher.final()]).toString();
         return decrypted;
     }
